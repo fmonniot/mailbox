d24c93467657e23cce22a1d65c09e4e4190b9d63
François Monniot <francoismonniot@gmail.com>
2014-11-17 14:55:32 +0100

    use better JSON parser

--- mailbox-manager/src/main/java/com/github/fmonniot/mailbox/config/ApplicationConfiguration.java
+++ mailbox-manager/src/main/java/com/github/fmonniot/mailbox/config/ApplicationConfiguration.java
@@ -1,10 +1,12 @@
 package com.github.fmonniot.mailbox.config;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ApplicationConfiguration extends ResourceConfig {
 
     public ApplicationConfiguration() {
+        register(JacksonFeature.class);
         register(new ApplicationBinder());
 
         packages(true, "com.github.fmonniot.mailbox.api");
